df73467ebb0496553c083f315937e1d304f58bda
Jack Clackett <jack@noquarter.co>
2024-01-24 19:38:28 +0100

    add followers

--- src/app/(tabs)/(index,feed,account)/[username]/followers.tsx
+++ src/app/(tabs)/(index,feed,account)/[username]/followers.tsx
@@ -1,10 +1,17 @@
 import { ScreenView } from "@/components/ScreenView"
+import { UserItem } from "@/components/UserItem"
+import { users } from "@/data/users"
 import { ScrollView } from "react-native"
 
 export default function Screen() {
+  const random5Users = users.sort(() => Math.random() - Math.random()).slice(0, 5)
   return (
     <ScreenView title="User's followers">
-      <ScrollView className="flex-1"></ScrollView>
+      <ScrollView className="flex-1">
+        {random5Users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </ScrollView>
     </ScreenView>
   )
 }
